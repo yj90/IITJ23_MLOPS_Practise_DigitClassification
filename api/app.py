e877305afc9553f6522efa3dab78f513f2993f4f
yj90 <ysjamdhade17@gmail.com>
2023-12-02 12:59:34 +0530

    added quiz4 code

--- api/app.py
+++ api/app.py
@@ -1,22 +1,28 @@
 from flask import Flask, request, jsonify
 from joblib import load
+import os
 
 app = Flask(__name__)
 
-@app.route('/hello/<name>')
-def index(name):
-    return "Hello, "+name+"!"
+@app.route('/')
+def index():
+    return "Hello, World"
 
-@app.route('/model', methods=['POST'])
+@app.route("/", methods=["POST"])
+def hello_world_post():    
+    return {"op" : "Hello, World POST " + request.json["suffix"]}
+
+@app.route('/predict', methods=['POST'])
 def pred_model():
     js = request.get_json()
-    image1 = js['image1']
-    image2 = js['image2']
+    image1 = [js['image']]
     #Assuming this is the path of our best trained model
-    model = load('/Users/pranjal/Desktop/ml-ops/models/svmgamma:0.001_C:1.joblib')
-    prediction_image_1 = model.predict(image1)
-    prediction_image_2 = model.predict(image2)
-    if(prediction_image_1 == prediction_image_2):
-        return "True"
-    else:
-        return "False"
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, '../models/treemax_depth:100.joblib')
+    model = load(filename)
+    pred1 = model.predict(image1)
+    #reurn pred1 in json
+    return jsonify(prediction=pred1.tolist())
+    
+if __name__ == '__main__':
+    app.run(debug=True)
