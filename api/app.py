4ae29231c14bd4ee44ec056b9853db3264a4b960
yj90 <ysjamdhade17@gmail.com>
2023-12-02 12:42:18 +0530

    Added quiz 3 code

--- api/app.py
+++ api/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from joblib import load
 
 app = Flask(__name__)
 
@@ -9,6 +10,13 @@ def index(name):
 @app.route('/model', methods=['POST'])
 def pred_model():
     js = request.get_json()
-    x= js['x']
-    y = js['y']
-    return x+y
+    image1 = js['image1']
+    image2 = js['image2']
+    #Assuming this is the path of our best trained model
+    model = load('/Users/pranjal/Desktop/ml-ops/models/svmgamma:0.001_C:1.joblib')
+    prediction_image_1 = model.predict(image1)
+    prediction_image_2 = model.predict(image2)
+    if(prediction_image_1 == prediction_image_2):
+        return "True"
+    else:
+        return "False"
