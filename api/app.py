fe8635f205f43c0792eb1afac1f28d41bc1a8d8b
yj90 <ysjamdhade17@gmail.com>
2023-12-02 19:10:50 +0530

    Added Q4

--- api/app.py
+++ api/app.py
@@ -1,8 +1,27 @@
 from flask import Flask, request, jsonify
 from joblib import load
 import os
+from markupsafe import escape
+
 
 app = Flask(__name__)
+
+def load_model(model_name):
+    dirname = os.path.dirname(__file__)
+    filename_svm = os.path.join(dirname, '../models/svmgamma:0.0001_C:1.joblib')
+    svm = load(filename_svm)
+    filename_tree = os.path.join(dirname, '../models/tree_max_depth:100.joblib')
+    tree = load(filename_tree)
+    filename_lr = os.path.join(dirname, '../models/M22AIE236_lr_lbfgs.joblib')
+    lr = load(filename_lr)
+    if model_name == 'svm':
+        return svm
+    elif model_name == 'tree':
+        return tree
+    elif model_name == 'lr':
+        return lr
+    else:
+        return None
 
 @app.route('/')
 def index():
@@ -12,14 +31,13 @@ def index():
 def hello_world_post():    
     return {"op" : "Hello, World POST " + request.json["suffix"]}
 
-@app.route('/predict', methods=['POST'])
-def pred_model():
+@app.route('/predict/<model_name>', methods=['POST'])
+def pred_model(model_name):
     js = request.get_json()
+    model_name = escape(model_name)
     image1 = [js['image']]
     #Assuming this is the path of our best trained model
-    dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, '../models/treemax_depth:100.joblib')
-    model = load(filename)
+    model = load_model(model_name)
     pred1 = model.predict(image1)
     #reurn pred1 in json
     return jsonify(prediction=pred1.tolist())
--- test_api.py
+++ test_api.py
@@ -1,9 +1,8 @@
 from api.app import app
 import pytest
 from utils import read_digits,preprocess_data
-import pandas as pd
 
-# Basic example testing added
+# Basic example testing 
 def test_get_root():
     response = app.test_client().get("/")
     assert response.status_code == 200
@@ -21,69 +20,21 @@ def get_processed_data():
     y = y[:100]
     X = preprocess_data(X)
     return X,y
-#main test cases for asseting digits prediction
-def test_predict_0():
-    X,y = get_processed_data()
-    response = app.test_client().post("/predict", json={"image":X[0].tolist()})
-    assert response.status_code == 200    
-    assert response.get_json()['prediction'] == [0]
 
-def test_predict_1():
+def test_predict_svm():
     X,y = get_processed_data()
-    response = app.test_client().post("/predict", json={"image":X[1].tolist()})
-    assert response.status_code == 200    
-    assert response.get_json()['prediction'] == [1]
-
-def test_predict_2():
-    X,y = get_processed_data()
-    response = app.test_client().post("/predict", json={"image":X[2].tolist()})
+    response = app.test_client().post("/predict/svm", json={"image":X[9].tolist()})
     assert response.status_code == 200    
     assert response.get_json()['prediction'] == [2]
 
-def test_predict_3():
+def test_predict_tree():
     X,y = get_processed_data()
-    response = app.test_client().post("/predict", json={"image":X[3].tolist()})
+    response = app.test_client().post("/predict/tree", json={"image":X[0].tolist()})
     assert response.status_code == 200    
-    assert response.get_json()['prediction'] == [3]
+    assert response.get_json()['prediction'] == [0]
 
-def test_predict_4():
+def test_predict_lr():
     X,y = get_processed_data()
-    response = app.test_client().post("/predict", json={"image":X[4].tolist()})
+    response = app.test_client().post("/predict/lr", json={"image":X[0].tolist()})
     assert response.status_code == 200    
-    assert response.get_json()['prediction'] == [4]
-
-def test_predict_5():
-    X,y = get_processed_data()
-    response = app.test_client().post("/predict", json={"image":X[5].tolist()})
-    assert response.status_code == 200    
-    assert response.get_json()['prediction'] == [5]
-
-def test_predict_6():
-    X,y = get_processed_data()
-    response = app.test_client().post("/predict", json={"image":X[6].tolist()})
-    assert response.status_code == 200    
-    assert response.get_json()['prediction'] == [6]
-
-def test_predict_7():
-    X,y = get_processed_data()
-    response = app.test_client().post("/predict", json={"image":X[7].tolist()})
-    assert response.status_code == 200    
-    assert response.get_json()['prediction'] == [7]
-
-def test_predict_8():
-    X,y = get_processed_data()
-    response = app.test_client().post("/predict", json={"image":X[8].tolist()})
-    assert response.status_code == 200    
-    assert response.get_json()['prediction'] == [8]
-
-def test_predict_9():
-    X,y = get_processed_data()
-    response = app.test_client().post("/predict", json={"image":X[9].tolist()})
-    assert response.status_code == 200    
-    assert response.get_json()['prediction'] == [9]
-
-def test_status_code():
-        response = app.test_client().get("/")
-        assert response.status_code == 200   
-        response = app.test_client().get("/random")
-        assert response.status_code == 404
+    assert response.get_json()['prediction'] == [0]
